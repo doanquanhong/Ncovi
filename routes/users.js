07ba7cb4a185405034998ca25f02a1c0a384445d
doanquanhong <hoangphong55555@gmail.com>
2021-03-02 19:48:20 +0700

    commit on 'master'

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 var passport = require('passport')
-const {check, validationResult} = require('express-validator')
 const User = require('../Models/User')
 
 /* GET Homepage. */
@@ -33,12 +32,14 @@ router.post('/register', async (req, res) => {
                 message: 'It Seems Like, This number is already, registered with  us!',
             })
         } else {
+          // Create a new user object to add to the DB.
             const user = new User( {
                 name: name,
                 number: number,
                 state: state,
             })
             const addedUser = await user.save()
+            // Saving the user to the database.
             res.redirect('/success')
         }
         } catch (error) {
@@ -60,37 +61,6 @@ router.get('/success', function(req, res, next) {
       hasErrors: messages.length > 0,
     })
   })
-
-
-
-// router.post('/register', async (req, res) => {
-//     try {
-//         const { name, number, state } = req.body
-
-//     const new_user = await User.aggregate([{
-//         $match: { number: number}
-//     }])
-
-//     if (new_user.length != 0) {
-//         res.render('already_registered', {
-//             message: 'It Seems Like, This number is already, registered with  us!',
-//         })
-//     } else {
-//         const user = new User( {
-//             name: name,
-//             number: number,
-//             state: state,
-//         })
-//         const addedUser = await user.save()
-//         res.redirect('/users/register/success')
-//     }
-//     } catch (error) {
-//         console.error();
-//         res.json({message: error})
-//     }   
-// })
-
-
 
 
 module.exports = router
